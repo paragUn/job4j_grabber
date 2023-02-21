2163c44e92efc065cb5bea9c9569e1dbd2e2523c
Roman <roman_stalmakov@mail.ru>
2023-02-21 11:50:03 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -9,18 +9,19 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
-//    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
     private static final int COUNT_OF_PAGES = 5;
 
     public static void main(String[] args) throws IOException {
         for (int i = 1; i <= COUNT_OF_PAGES; i++) {
-            String pageLink = String.format("%s/vacancies/java_developer?page=%d", SOURCE_LINK, i);
-            Connection  connection = Jsoup.connect(pageLink.concat(String.valueOf(i)));
+            String pageLink = String.format("%s%d", PAGE_LINK, i);
+            Connection  connection = Jsoup.connect(String.format("%s%s", pageLink, "?page=" + i));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
@@ -31,22 +32,19 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 LocalDateTime date = new HabrCareerDateTimeParser().parse(dateElement.attr("datetime"));
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", date, vacancyName, link);
-//                try {
-//                    System.out.printf(retrieveDescription(link));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
+                System.out.printf("%s %s %s %n%s%n", date, vacancyName, link, retrieveDescription(link));
             });
         }
     }
-    private static String retrieveDescription(String link) throws IOException {
-        Connection connectionDetail = Jsoup.connect(link);
-        Document document = connectionDetail.get();
-        Elements descriptionElements = document.select(".vacancy-description__text");
-        descriptionElements.forEach(row -> {
-            Element descriptionElement = row.select(".vacancy-description__text").text();
-                });
-        return descriptionElement.text();
+    private static String retrieveDescription(String link) {
+        String result = null;
+        try {
+            Document document = Jsoup.connect(link).get();
+            Element descriptionElement = document.selectFirst(".vacancy-description__text");
+            result = Objects.requireNonNull(descriptionElement.text());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
