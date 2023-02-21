282d066cbabcbbc67fc6db9db42d4d36ab6ccf26
Roman <roman_stalmakov@mail.ru>
2023-02-21 14:18:15 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,37 +5,52 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class HabrCareerParse {
-
+public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
-
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
     private static final int COUNT_OF_PAGES = 5;
 
-    public static void main(String[] args) throws IOException {
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> listOfPosts = new ArrayList<>();
         for (int i = 1; i <= COUNT_OF_PAGES; i++) {
             String pageLink = String.format("%s%d", PAGE_LINK, i);
-            Connection  connection = Jsoup.connect(String.format("%s%s", pageLink, "?page=" + i));
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                Element innerElement = row.select(".vacancy-card__date").first();
-                Element dateElement = innerElement.child(0);
-                String vacancyName = titleElement.text();
-                LocalDateTime date = new HabrCareerDateTimeParser().parse(dateElement.attr("datetime"));
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s %n%s%n", date, vacancyName, link, retrieveDescription(link));
-            });
+                Connection connection = Jsoup.connect(String.format("%s%s", pageLink, "?page=" + i));
+            try {
+                Document document = connection.get();
+                Elements rows = document.select(".vacancy-card__inner");
+                rows.forEach(row -> {
+                    Element titleElement = row.select(".vacancy-card__title").first();
+                    Element dateElement = row.select(".vacancy-card__date").first().child(0);
+                    String vacancyName = titleElement.text();
+                    LocalDateTime date = new HabrCareerDateTimeParser().parse(dateElement.attr("datetime"));
+                    String vacancyLink = String.format("%s%s", link, titleElement.child(0).attr("href"));
+                    String description = retrieveDescription(vacancyLink);
+                    System.out.printf("%s %s %s %n%s%n", date, vacancyName, vacancyLink, description);
+                    listOfPosts.add(new Post(vacancyName, vacancyLink, description, date));
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return listOfPosts;
     }
+
     private static String retrieveDescription(String link) {
         String result = null;
         try {
--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -18,6 +18,13 @@ public class Post {
         this.created = created;
     }
 
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
